8476feafd5f0459fa7d33638766112cf44424372
Todd Parson <todd.parson@harness.io>
2026-03-01 20:58:27 -0600

    removing unnecessary VPC output

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -68,11 +68,6 @@ output "subnet_ids" {
   value       = aws_subnet.public[*].id
 }
 
-output "private_subnet_ids" {
-  description = "IDs of the private subnets"
-  value       = aws_subnet.private[*].id
-}
-
 output "app_security_group_id" {
   description = "ID of the application security group"
   value       = aws_security_group.app.id
